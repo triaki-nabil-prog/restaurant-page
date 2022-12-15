1883657099f4ca0f548ba6533c27dbf0f3f34802
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-12-15 10:33:07 +0100

    added html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -7,6 +8,11 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+      plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
     module: {
         rules: [
             {
