abf04697c742bbccb52e25e6f21b7afb1db1e39c
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-12-15 10:34:17 +0100

    added dist clean option

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,13 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
-      plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
     module: {
         rules: [
             {
